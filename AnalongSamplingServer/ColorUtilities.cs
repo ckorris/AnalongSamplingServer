ff8b34180a3a2302a876961a71c3ac1585d3f956
Chris Orris <ckorris@gmail.com>
2020-12-13 14:31:50 -0600

    Colors now persist on devices.

---
+++ AnalongSamplingServer/ColorUtilities.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    public static class ColorUtilities
+    {
+        private static List<Color> _colors = new List<Color>()
+        {
+            Color.Blue,
+            Color.Red,
+            Color.Green,
+            Color.Magenta,
+            Color.Orange,
+            Color.Yellow,
+            Color.Cyan,
+            Color.DeepPink,
+            Color.LightSeaGreen,
+            Color.Purple,
+            Color.Navy,
+            Color.Aquamarine,
+            Color.Chocolate,
+            Color.Silver,
+            Color.Crimson,
+            Color.SlateGray,
+            Color.Honeydew,
+            Color.SpringGreen,
+            Color.SteelBlue,
+            Color.OrangeRed
+        };
+
+        private static int _colorIncrementIndex = 0;
+
+        public static Color GetColorByIndex(int colorIndex)
+        {
+            return _colors[colorIndex % _colors.Count];
+        }
+
+        public static Color GetSequentialColor()
+        {
+            Color returnColor = _colors[_colorIncrementIndex];
+
+            _colorIncrementIndex++;
+            if(_colorIncrementIndex >= _colors.Count)
+            {
+                _colorIncrementIndex = 0;
+            }
+
+            return returnColor;
+        }
+    }
+}
--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -59,7 +59,10 @@ namespace UI
         {
             private MainWindow _window;
             //private Plottable _last;
-            private List<Plottable> _last = new List<Plottable>();
+            //private List<Plottable> _last = new List<Plottable>();
+
+            private Dictionary<int, Plottable> devicePlots = new Dictionary<int, Plottable>();
+            private Dictionary<int, List<ushort>> deviceSamples = new Dictionary<int, List<ushort>>();
 
             public GraphDataSink (MainWindow window)
             {
@@ -83,38 +86,35 @@ namespace UI
 
                 if (packet.SampleID == 0) //Could be done better, like a flag.
                 {
-                    ClearLastPlots();
+                    ClearAllDataAndPlots();
                 }
 
+                //TODO: This updates all previous samples with the same overall sample rate. They should be equally accurate in theory, but errors will present incorrectly. Fix.
                 double sampleDuration = packet.EndTimeUs - packet.StartTimeUs;
                 double microSecondsPerSample = sampleDuration / packet.SampleCount; 
-                //double milliSecondsPerSample = microSecondsPerSample / 1000d;
                 double sampleRateMS = 1000d / microSecondsPerSample;
 
-                double xOffsetMs = packet.StartTimeUs / 1000d;
-
-                PlottableSignal newPlot = plt.PlotSignal(ToDouble(packet.Samples), sampleRate: sampleRateMS, label: "Device " + packet.DeviceID.ToString(), xOffset: xOffsetMs);
-                plt.Resize();
-                _last.Add(newPlot);
-
-                plt.Legend();
-                _window.TheGraph.Render();
-            }
-
-            private void IngestMainThread(List<SamplePacket> packets)
-            {
-                var plt = _window.TheGraph.plt;
-
-                ClearLastPlots();
-
-                for (int i = 0; i < packets.Count; i++)
+                //Clear the previous plot so that we can extend it with the old and new data combined. 
+                if(devicePlots.ContainsKey(packet.DeviceID))
                 {
-                    SamplePacket packet = packets[i]; //Shorthand.
-                    PlottableSignal newPlot = plt.PlotSignal(ToDouble(packet.Samples), yOffset: i * 500, label: "Device " + packet.DeviceID.ToString());
-                    plt.Resize();
-
-                    _last.Add(newPlot);
+                    _window.TheGraph.plt.Remove(devicePlots[packet.DeviceID]);
                 }
+
+                //If we haven't yet made a list for the data for this device ID, make a new one. 
+                if(deviceSamples.ContainsKey(packet.DeviceID) == false)
+                {
+                    deviceSamples.Add(packet.DeviceID, new List<ushort>());
+                }
+
+                //Add the new data to the old.
+                deviceSamples[packet.DeviceID].AddRange(packet.Samples.ToList());
+
+                //Plot all the data up to this point for the given device ID.
+                //PlottableSignal newPlot = plt.PlotSignal(ToDouble(packet.Samples), sampleRate: sampleRateMS, label: "Device " + packet.DeviceID.ToString());
+                System.Drawing.Color plotColor = ColorUtilities.GetColorByIndex(packet.DeviceID);
+                PlottableSignal newPlot = plt.PlotSignal(ToDouble(deviceSamples[packet.DeviceID].ToArray()), sampleRate: sampleRateMS, label: "Device " + packet.DeviceID.ToString(), color: plotColor);
+                plt.Resize();
+                devicePlots[packet.DeviceID] = newPlot;
 
                 plt.Legend();
                 _window.TheGraph.Render();
@@ -130,17 +130,16 @@ namespace UI
                 return result;
             }
 
-            private void ClearLastPlots()
+            private void ClearAllDataAndPlots()
             {
-                if (_last != null)
+                foreach(Plottable plottable in devicePlots.Values)
                 {
-                    Plot plt = _window.TheGraph.plt;
-
-                    for (int i = 0; i < _last.Count; i++)
-                    {
-                        plt.Remove(_last[i]);
-                    }
+                    _window.TheGraph.plt.Remove(plottable);
                 }
+
+                devicePlots.Clear();
+
+                deviceSamples.Clear();
             }
         }
 
